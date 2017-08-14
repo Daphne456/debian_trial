f8fdaba02020c14172d48f509f77079ce79bdab0
Daphne456 <jewickens@hotmail.com>
2017-08-15 02:53:28 +0700

    Update debian7.sh

--- Update/debian7.sh
+++ Update/debian7.sh
@@ -110,7 +110,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf $source/nginx.conf
 mkdir -p /home/vps/public_html
-echo "<pre>Modified by Yuri Bhuana</pre>" > /home/vps/public_html/index.html
+echo "<pre>Modified by Yujin Krabs</pre>" > /home/vps/public_html/index.html
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
 wget -O /etc/nginx/conf.d/vps.conf $source/vps.conf
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
